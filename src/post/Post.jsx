0c6cbc723614af891beaaf38a57d8b5ce45b48ab
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-08-08 15:16:18 -0500

    dfsfdsds

---
+++ src/post/Post.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img className ="img" src="https://img.freepik.com/free-photo/reflection-lights-mountain-lake-captured-parco-ciani-lugano-switzerland_181624-24209.jpg?w=1380&t=st=1691521241~exp=1691521841~hmac=eb4a987bfa47aa8574d907616f80303cd8c67978405870794e14e9d96147c34b"
+        alt=""> 
+        </img>
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            </span>
+           {/* hr/ adds a line */}
+            <hr/>
+            <span className="postDate"> 1 hour ago</span>
+
+        </div>
+
+
+    </div>
+  )
+}
--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./posts.css"
+import Post from '../post/Post.jsx'
+
 
 export default function Posts() {
   return (
     <div className="posts">
-        posts
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+
       
     </div>
   )
