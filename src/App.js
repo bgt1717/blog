b2d3e48528f57329b7f9510742008f602d970c09
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-08-09 22:42:08 -0500

    another commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 
@@ -9,7 +10,9 @@ function App() {
     <>
         <TopBar/>
         {/* <p>test</p> */}
-        <Single/>
+        
+        <Write/>
+        
         
       
     </>
--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import "./write.css"
 
 export default function Write() {
   return (
     <div>
         <div className="write">
-            asd
+          <img
+            className="writeImg"
+            src ="https://mediablog.cdnpk.net/sites/9/2021/12/landscape-cabacera-new.jpg"
+            alt=""
+
+          />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                    <i className= "writeIcon fa-solid fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: "none"}}/>
+                    <input type= "text" placeholder="Title" className="writeInput" autoFocus={true}/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Tell your story..." 
+                              type="text" 
+                              className="writeInput writeText"
+                    ></textarea>
+
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
         </div>
       
     </div>
