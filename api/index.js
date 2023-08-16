93dcb9bdfc3d787577b849b9e8208e7e325977ec
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-08-15 20:38:36 -0500

    DB. Problems... :/

--- api/index.js
+++ api/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-console.log("hi")
+dotenv.config();
 
-app.listen("5000", ()=>{
-console.log("backend is running.");
-
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
