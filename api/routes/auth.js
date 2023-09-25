6c81e0b783eac8a0503d0c5e2c54540bd67a0ead
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-09-25 15:30:46 -0500

    29:00

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register", async (req, res)=>{
     try{
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
