04ec41a81847a4186a1e28bb416d7434105f6738
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-09-25 17:30:00 -0500

    45:00

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/User");
 const bcrypt = require("bcrypt");
 
 
@@ -32,17 +33,20 @@ router.put("/:id", async (req, res)=>{
 
 router.put("/:id", async (req, res)=>{
     if(req.body.userId === req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            try{
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted.");
 
-    try{
-        //HERE
-        res.status(200).json(updatedUser);
-
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    }
-    else{
-        res.status(401).json("You can delete only your account!");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+            } catch(err) {
+                res.status(404).json("user not found")
+            }
+        } else{
+            res.status(401).json("You can delete only your account!");
     }
 });
 
