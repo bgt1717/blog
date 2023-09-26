5d7f54bd14827ed68c157919ee69563384f6a1f1
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-09-26 15:01:44 -0500

    56.35

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -18,23 +18,28 @@ router.post("/", async (req, res)=>{
 
 //UPDATE POST
 
-router.delete("/:id", async (req, res)=>{
-    if(req.body.userId === req.params.id){
-        try{
-            const user = await User.findById(req.params.id);
+router.put("/:id", async (req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.username === req.body.username) {
             try{
-                await Post.deleteMany({username:user.username});
-                await User.findByIdAndDelete(req.params.id)
-                res.status(200).json("User has been deleted.");
-
-            } catch (err) {
-                res.status(500).json(err);
-            }
-            } catch(err) {
-                res.status(404).json("user not found")
+                const updatedPost = await Post.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set:req.body
+                }, 
+                { new: true }
+            );
+            res.status(200).json(updatedPost);
+            } catch(err){
+                res.status(500).json(err)
             }
         } else{
-            res.status(401).json("You can delete only your account!");
+            res.status(401).json("You can only update your post!")
+        }
+ 
+    } catch(err){
+        res.status(500).json(err)
     }
 });
 
