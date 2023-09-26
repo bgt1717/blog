637c5dcb5cd4f1e523d0a2422d190acb62aa8ef1
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-09-26 14:07:23 -0500

    user router complete

--- api/routes/users.js
+++ api/routes/users.js
@@ -57,7 +57,7 @@ router.get("/:id", async (req,res)=>{
     try{
         const user = await User.findById(req.params.id);
         const {password, ...others} = user._doc;
-        res.status(200).json(user);
+        res.status(200).json(others);
     }catch(err){
         res.status(500).json(err)
     }
