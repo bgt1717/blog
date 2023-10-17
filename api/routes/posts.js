e6e5e4226794424b7d1ef00db70a8ba924cdec5a
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-10-17 01:03:56 -0500

    posts completed 1:04

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -68,12 +68,35 @@ router.delete("/:id", async (req, res) => {
 //GET POST
 router.get("/:id", async (req,res)=>{
     try{
-        const user = await User.findById(req.params.id);
-        const {password, ...others} = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     }catch(err){
         res.status(500).json(err)
     }
 })
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+      let posts;
+      if (username) {
+        posts = await Post.find({ username });
+      } else if (catName) {
+        posts = await Post.find({
+          categories: {
+            $in: [catName],
+          },
+        });
+      } else {
+        posts = await Post.find();
+      }
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
