c2319d8510c9cf68e6ff86639965d619d6d76f1b
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-10-30 14:15:54 -0500

    17.40

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import "./sidebar.css"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(()=>{
+    const getCats = async ()=>
+    {
+      const res = await axios.get("/categories")
+      setCats(res.data)
+    }
+    getCats();
+  }, [])
   return (
     <div className = "sidebar">
         <div className="sidebarItem">
@@ -17,13 +29,11 @@ export default function Sidebar() {
         <div className="sidebarItem">
           <span className="sidebarTitle">CATEGORIES</span>
           <ul className="sidebarList">
-            <li className="sidebarListItem">Life</li>
-            <li className="sidebarListItem">Music</li>
-            <li className="sidebarListItem">Style</li>
-            <li className="sidebarListItem">Sport</li>
-            <li className="sidebarListItem">Tech</li>
-            <li className="sidebarListItem">Cinema</li>
+            {cats.map((c)=> (
+              <li className="sidebarListItem">{c.name}</li>
+            ))}
 
+            
 
           </ul>
         </div>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,10 +5,13 @@ import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 import  "./home.css";
 import axios from "axios"
+import { useLocation } from 'react-router-dom';
 
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const location = useLocation();
+  console.log(location)
 
   useEffect(()=>{
     const fetchPosts = async ()=>{
