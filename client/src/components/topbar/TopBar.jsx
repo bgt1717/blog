c59ab060239929e495f870472e0c628cfca84295
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-11-07 12:02:23 -0600

    59:00

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -33,7 +33,7 @@ export default function TopBar() {
             {
                 user ? (
                     <img className = "topImg"
-                    src="https://res.cloudinary.com/simpleview/image/upload/v1648755098/clients/austin/Austin_Skyline_Credit_Christopher_Sherman_lifetime__4f60343d-9f69-450c-8ad3-fa636761786d.jpg" 
+                    src={user.profilePicture} 
                     alt = "" >
                 </img>
                 ) : (
