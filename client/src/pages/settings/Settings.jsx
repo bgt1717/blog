582d45f2d1d9e0eb2a52bae6f44740e8490e8bdd
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-11-07 20:10:14 -0600

    1:45

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -12,8 +12,8 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
-
+  
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "UPDATE_START" });
