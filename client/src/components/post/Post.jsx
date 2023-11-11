c34957ff183ee28de00e7d1d7896dc31e2000ece
bryce <bgt17@txstate.edu>
2023-11-10 19:52:10 -0600

    update

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,27 +2,31 @@ import React from 'react'
 import "./post.css"
 import { Link } from 'react-router-dom'
 
-export default function Post({post}) {
+export default function Post({ post }) {
   const PF = "http://localhost:5000/images/";
+
+  // Truncate description to 50 characters
+  const truncatedDesc = post.desc.length > 50 ? `${post.desc.substring(0, 50)}...` : post.desc;
+
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
-        <div className="postInfo">  
-            <div className="postCats">
-                {post.categories.map((c) => (
-                  <span className="postCat">{c.name}</span>
-                ))}
-            </div>
-            <Link to={`/post/${post._id}`} className="link" >
-            <span className="postTitle">{post.title}</span>
-            </Link>
-           {/* hr/ adds a line */}
-            <hr/>
-            <span className="postDate">
-                {new Date(post.createdAt).toDateString()}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCat" key={c.name}>
+              {c.name}
             </span>
+          ))}
         </div>
-        <p className = "postDesc">{post.desc}</p> 
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+      </div>
+      <p className="postDesc">{truncatedDesc}</p>
     </div>
-  )
+  );
 }
+
