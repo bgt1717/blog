2aed5ca0e27c362e9ba7f7ed309c1e28f407f957
bryce <bgt17@txstate.edu>
2023-11-10 19:13:43 -0600

    footer updated

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
   // }, [])
   return (
     <div className = "sidebar">
-        <div className="sidebarItem">
+        {/* <div className="sidebarItem">
           <span className="sidebarTitle"> ABOUT ME </span>
           <img className ="img" src="https://img.freepik.com/free-photo/reflection-lights-mountain-lake-captured-parco-ciani-lugano-switzerland_181624-24209.jpg?w=1380&t=st=1691521241~exp=1691521841~hmac=eb4a987bfa47aa8574d907616f80303cd8c67978405870794e14e9d96147c34b"
           alt = ""
@@ -26,7 +26,7 @@ export default function Sidebar() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
           </p>
-        </div>
+        </div> */}
         {/* <div className="sidebarItem">
           <span className="sidebarTitle">CATEGORIES</span>
           <ul className="sidebarList">
