7a5fafd7ddb0352606eec2c999d897c5dcb4612c
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-11-14 17:57:36 -0600

    about updated

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
+import greetingsImage from './greetings.jpg';
 
 export default function About() {
+  const containerStyle = {
+    backgroundImage: `url(${greetingsImage})`,
+  };
+
   return (
-    <div>
-      hi
+    <div style={containerStyle} className="about-container">
+      <h2>About Us</h2>
+      <p className="about-paragraph">
+        Welcome to our platform! Here, you can share your thoughts and ideas by creating posts.
+        To get started, simply create an account, and you'll be able to contribute and engage
+        with our community. We look forward to hearing from you!
+      </p>
     </div>
-  )
+  );
 }
