468fee0f67ca700dc265468f1c0b051862a620d8
bryce <bgt17@txstate.edu>
2023-11-15 15:14:48 -0600

    settings updated

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,5 +1,4 @@
 import "./settings.css";
-// import Sidebar from "../../components/sidebar/Sidebar";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
 import axios from "axios";
@@ -12,27 +11,41 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
- 
+  const PF = "http://localhost:5000/images/";
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "UPDATE_START" });
+
     const updatedUser = {
       userId: user._id,
-      username,
-      email,
-      password,
     };
+
+    // Include fields that have been changed
+    if (username) {
+      updatedUser.username = username;
+    }
+    if (email) {
+      updatedUser.email = email;
+    }
+    if (password) {
+      updatedUser.password = password;
+    }
+
     if (file) {
       const data = new FormData();
       const filename = Date.now() + file.name;
       data.append("name", filename);
       data.append("file", file);
       updatedUser.profilePicture = filename;
+
       try {
         await axios.post("/upload", data);
-      } catch (err) {}
+      } catch (err) {
+        // Handle the error if necessary
+      }
     }
+
     try {
       const res = await axios.put("/users/" + user._id, updatedUser);
       setSuccess(true);
