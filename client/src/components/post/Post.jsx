feb68bc725d59814c9db24485da89d0393a97cca
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-11-27 22:10:46 -0600

    backend working so far

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import "./post.css"
 import { Link } from 'react-router-dom'
 
 export default function Post({ post }) {
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://blogbackend-6zuh.onrender.com/images/";
 
   // Truncate description to 200 characters
   const truncatedDesc = post.desc.length > 200 ? `${post.desc.substring(0, 200)}...` : post.desc;
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -15,7 +15,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try{
-        const res = await axios.post("/auth/register", {
+        const res = await axios.post("https://blogbackend-6zuh.onrender.com/auth/register", {
           username,
           email,
           password,
